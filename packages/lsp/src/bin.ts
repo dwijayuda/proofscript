2567c1d30923293c023302dc4cdad8b729842ecc
Dwija Yuda <dwijayuda@gmail.com>
2026-09-22 21:00:19 +0700

    fix: type LSP CLI arguments explicitly

--- packages/lsp/src/bin.ts
+++ packages/lsp/src/bin.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import { startLspServer } from "./index.js";
 
-const cliArgs = process.argv.slice(2) as string[];
-const debounceArg = cliArgs.find((arg: string) => arg.startsWith("--diagnostics-debounce-ms="));
+const argv: string[] = Array.isArray(process.argv) ? process.argv.slice(2).map(String) : [];
+const debounceArg = argv.find((arg: string) => arg.startsWith("--diagnostics-debounce-ms="));
 const diagnosticsDebounceMs = debounceArg
   ? Number(debounceArg.slice(debounceArg.indexOf("=") + 1))
   : undefined;
