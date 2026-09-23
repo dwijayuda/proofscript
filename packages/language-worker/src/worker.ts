c672f9ef66450b40cff91efa4d6e1e13adde8a20
Dwija Yuda <dwijayuda@gmail.com>
2026-09-23 17:09:24 +0700

    editor: dispatch bounded code actions

--- packages/language-worker/src/worker.ts
+++ packages/language-worker/src/worker.ts
@@ -105,6 +105,8 @@ function dispatch(operation: string, args: any, cancellation?: CancellationToken
       return service.semanticTokens(args.uri, cancellation);
     case "formatDocument":
       return service.formatDocument(args.uri, cancellation);
+    case "codeActions":
+      return service.codeActions(args.uri, args.range, cancellation);
     case "goals":
       return service.goals(args.uri, args.position, cancellation);
     case "definition":
